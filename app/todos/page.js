f18733eb64414979150d54860ab75398efc8d6a0
MonaSoliman009 <monasoliman009@gmail.com>
2025-07-08 20:21:55 +0300

    fix(todos):comment todos logiv

--- app/todos/page.js
+++ app/todos/page.js
@@ -41,12 +41,12 @@
 import { GET } from "../api/todos/route"
 
 export default async function page() {
-    let res = await GET();
-    let todos = await res.json()
+    // let res = await GET();
+    // let todos = await res.json()
     return (
         <>
             <div className="grid grid-cols-4 gap-3">
-                {todos.map((todo) =>
+                {/* {todos.map((todo) =>
 
                     <div key={todo._id} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
                         <div className="p-5">
@@ -56,7 +56,7 @@ export default async function page() {
                             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 line-clamp-1">{todo.status}</p>
                         </div>
                     </div>
-                )}
+                )} */}
             </div>
 
         </>
